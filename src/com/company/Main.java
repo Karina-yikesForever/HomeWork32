c6cb79d7fa4b68225b18177fa3c5aaef4f8af14f
Karina <karinakokosova8@gmail.com>
2019-12-03 15:05:52 +0600

    File

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.awt.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+FileWriter fileWriter1=new FileWriter("Song.txt");
+FileReader fileReader=new FileReader("File.txt");
+Scanner sc=new Scanner(fileReader);
     }
+}
+
+
+class Song {
+    String name;
+    String singer;
+    String genre;
+    String lyrics;
+
+    public Song(String name, String singer, String genre, String lyrics) {
+        this.name = name;
+        this.singer = singer;
+        this.genre = genre;
+        this.lyrics = lyrics;
+    }
+    
 }
